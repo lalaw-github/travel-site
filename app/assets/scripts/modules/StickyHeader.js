fcce39d8d30b7f764ccfde8d064abf86df15c676
Lee <lawson831@aol.com>
2018-09-03 13:25:54 +0100

    completed lazyloading of images

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -5,7 +5,7 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 class StickyHeader {
 	
 	constructor() {
-		
+		this.lazyImages = $('.lazyload');
 		this.siteHeader = $('.site-header');
 		this.headerTriggerElement = $('.large-hero__title');
 		this.createHeaderWaypoint();
@@ -13,7 +13,14 @@ class StickyHeader {
 		this.headerLinks = $('.primary-nav a');
 		this.createPageSectionWaypoints();
 		this.addSmoothScroll();
+		this.refreshWaypoints();
 		
+	}
+	
+	refreshWaypoints() {
+		this.lazyImages.on('load', function() {
+			Waypoint.refreshAll();
+		});
 	}
 	
 	addSmoothScroll() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
  
 module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'App.js',
-        path: path.resolve(__dirname, './app/temp/scripts')
-    }, 
-		module: {
-			rules: [
-				{
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015']
-					},
-					test: /\.js$/, 
-					exclude: /node_modules/
-				}
-			]
-		}
+	entry: {
+		App: './app/assets/scripts/App.js',
+		Vendor: './app/assets/scripts/Vendor.js'
+	},
+	output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, './app/temp/scripts')
+	}, 
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				},
+				test: /\.js$/, 
+				exclude: /node_modules/
+			}
+		]
+	}
 };
